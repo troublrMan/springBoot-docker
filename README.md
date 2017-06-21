ec0f92811c03139949dd78a5ea617b3b0651e1cf
troubleMan <cxiangrikui@163.com>
2017-06-21 10:01:31 +0800

    springboot和dockers的整合

---
+++ README.md
@@ -0,0 +1,4 @@
+# springboot-docker-demo
+Docker构建、发布和运行Spring Boot应用Demo
+
+文章链接：
---
+++ src/main/java/com/anxpp/demo/DockerApplication.java
@@ -0,0 +1,16 @@
+package com.anxpp.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+@RestController
+public class DockerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApplication.class, args);
+	}
+	@RequestMapping
+	public String index(){
+		return "Hello 5451颯颯是國行價格回家沸騰魚伏特加提意見51world !";
+	}
+}
